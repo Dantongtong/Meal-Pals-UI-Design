a01b99f721a28e1733613f3f5a47f84368624a7b
Dantongtong <dz2451@cumc.columbia.edu>
2022-12-17 23:44:14 +0800

    finish create page

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -5,6 +5,7 @@ import dayjs from 'dayjs';
 import { InputNumber, DatePicker, TimePicker, Input, Switch } from 'antd';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
 
 dayjs.extend(customParseFormat);
@@ -13,52 +14,68 @@ const { TextArea } = Input;
 export default function Create() {
     const switchControl = {
         display: 'flex',
-        'justify-content': 'space-between'
+        'justify-content': 'space-between',
+        'margin-bottom': '20px'
     };
     const inputTitle = {
         'font-weight': '600',
         'font-size': '20px',
         'line-height': '24px',
         'letter-spacing': '0.38px',
-        'padding': '26px 0 20px'
+        'padding': '16px 0 20px'
     };
     const banner = {
         'text-align': 'center',
         'background': '#FFFFFF',
         'border-bottom': '1px solid #EEEEEE',
         'padding': '10px 12px',
-        'font-size': '18px'
+        'font-size': '18px',
+        'margin-bottom': '15px'
     };
+    const subtitle = {
+        'padding-bottom': '5px'
+    }
     return (
         <div>
             <UpperFrame />
             <div style={banner}>Create an Event</div>
-            <div style={inputTitle}>Event Information</div>
-            <div>Date and Time</div>
-            <DatePicker />
-            <TimePicker defaultOpenValue={dayjs('00:00:00', 'HH:mm:ss')} />
 
-            <div>
-                <div>Maximum Number of Participants
-                    (not including yourself)</div>
-                <InputNumber min={1} max={10} defaultValue={3} />
+            <div style={{ padding: '0 20px' }}>
+                <div style={inputTitle}>Event Information</div>
+                <div>
+                    <div style={subtitle}>Date and Time</div>
+                    <DatePicker />
+                    <TimePicker defaultOpenValue={dayjs('00:00:00', 'HH:mm:ss')} />
+                    <div style={banner}></div>
+                </div>
+
+
+                <div>
+                    <div>Maximum Number of Participants</div>
+                    <div style={subtitle}>(not including yourself)</div>
+                    <InputNumber min={1} max={10} defaultValue={3} />
+                    <div style={banner}></div>
+                </div>
+
+                <div>
+                    <div style={subtitle}>Event Description</div>
+                    <TextArea rows={4} />
+                    <div style={banner}></div>
+                </div>
+
+                <div style={switchControl}>
+                    <div>Need Permission to Join</div>
+                    <Switch defaultChecked />
+                </div>
+
+                <div style={switchControl}>
+                    <Button variant="contained" component={Link} to='/post'>Previous</Button>
+                    <Button variant="contained">Next</Button>
+                </div>
+                <LowerFrame />
             </div>
 
-            <div>
-                <div>Event Description</div>
-                <TextArea rows={4} />
-            </div>
 
-            <div style={switchControl}>
-                <div>Need Permission to Join</div>
-                <Switch defaultChecked/>
-            </div>
-
-            <div style={switchControl}>
-                <Button variant="contained">Previous</Button>
-                <Button variant="contained">Next</Button>
-            </div>
-            <LowerFrame />
         </div >
     );
 }
