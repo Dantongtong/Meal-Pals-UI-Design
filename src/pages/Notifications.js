29babe50615ee8addc824e6a6757a9ef35283981
apumaparna <aparnakumargs@gmail.com>
2022-12-19 07:14:35 -0500

    intro message card

--- src/pages/Notifications.js
+++ src/pages/Notifications.js
@@ -20,8 +20,31 @@ export default function Notifications() {
             <Box sx={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', fontWeight: 'bold' }}>
                 <h2>Notifications</h2>
             </Box>
-           
+            <Box sx={{ padding: 1, border: '1px #D4D4D5 solid', alignItems: 'center', fontWeight: 'bold' }}>
+                <h1>Upcoming</h1>
+                <UpcomingNotificationCard name="Kiley" location="Shaking Crab" />
+                <UpcomingNotificationCard name="Eve" location="Thai Market" />
+            </Box>
+            <Box sx={{ padding: 1, border: '1px #D4D4D5 solid', alignItems: 'center', fontWeight: 'bold' }}>
+                <h1>Past</h1>
+            </Box>
+
             <LowerFrame />
         </div >
     );
+}
+
+function UpcomingNotificationCard(props) {
+    return (
+        <div>
+            <Box sx={{padding: 1, display: 'flex', alignItems: 'center', border: '1px #D4D4D5 solid', 'border-radius': '8px' }}>
+                <Box sx={{ margin: 1 }}>
+                    <Avatar src="https://pbs.twimg.com/profile_images/877631054525472768/Xp5FAPD5_reasonably_small.jpg" />
+                </Box>
+                <Box sx={{ width: '50%', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <Typography><b>{props.name}</b> wants to join your event at <b>{props.location}.</b></Typography>
+                </Box>
+            </Box>
+        </div >
+    )
 }
