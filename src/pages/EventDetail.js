6d518b0ede0f8a3274b28aff815e8f75919667d6
apumaparna <aparnakumargs@gmail.com>
2022-12-19 22:44:01 -0500

    joined / remaining

--- src/pages/EventDetail.js
+++ src/pages/EventDetail.js
@@ -10,9 +10,15 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import TextField from '@mui/material/TextField';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import IconButton from '@mui/material/IconButton';
+import StarIcon from '@mui/icons-material/Star';
+import AvatarGroup from '@mui/material/AvatarGroup';
+import FaceIcon from '@mui/icons-material/Face';
+
 
 export default function EventDetail() {
-    const { organizer, location, time, joined, left} = useParams();
+    const { organizer, location, time, joined, left } = useParams();
+    const joinedInt = parseInt(joined, 10)
+    const leftInt = parseInt(left, 10)
 
     const Header = {
         'font-size': '18px',
@@ -25,11 +31,17 @@ export default function EventDetail() {
         'line-height': '24px'
     };
 
+    const joined_left = {
+        color: '#FEA600',
+        'font-size': '20px',
+        'line-height': '24px'
+    };
+
     return (
         <div>
             <Stack sx={{ margin: 2, width: '90%', display: 'flex' }}>
                 <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                    <Button startIcon={<ArrowBackIcon />} component={Link} to='/notifications'>
+                    <Button startIcon={<ArrowBackIcon />} component={Link} to='/'>
                         Back
                     </Button>
                     <IconButton aria-label="favorite" size="small">
@@ -53,14 +65,35 @@ export default function EventDetail() {
                 </Stack>
                 <Box sx={{ display: 'flex', alignItems: 'center' }}>
                     <Box sx={{ margin: 1 }}>
-                        <Avatar src="https://pbs.twimg.com/profile_images/877631054525472768/Xp5FAPD5_reasonably_small.jpg" 
-                        sx={{ width: 60, height: 60 }} />
+                        <Avatar src="https://pbs.twimg.com/profile_images/877631054525472768/Xp5FAPD5_reasonably_small.jpg"
+                            sx={{ width: 60, height: 60 }} />
                     </Box>
                     <Stack sx={{ width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'left' }}>
                         <div style={description}>{organizer}</div>
                         <div style={description}>M.S. in Operations Research</div>
                     </Stack>
                 </Box>
+                <Stack sx={{ mt: 2 }}>
+                    <div style={joined_left}><u>{joined} joined / {left} remaining</u></div>
+                </Stack>
+
+                <Box sx={{ margin: 2, width: '90%', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                    <AvatarGroup total={joinedInt}>
+                        {Array.from({ length: joinedInt }, (_, i) =>
+                            <span key={i}>
+                                <Avatar src="https://pbs.twimg.com/profile_images/877631054525472768/Xp5FAPD5_reasonably_small.jpg" />
+                            </span>)}
+                    </AvatarGroup>
+                    <AvatarGroup total={leftInt}>
+                        {Array.from({ length: leftInt }, (_, i) =>
+                            <span key={i}>
+                                <Avatar>
+                                    <FaceIcon />
+                                </Avatar>
+                            </span>)}
+                    </AvatarGroup>
+                </Box>
+
             </Stack>
 
         </div >
